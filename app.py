f3ba3637f31f956dd522111aed1484ca7979e87b
zooro-07 <suryavs2008@gmail.com>
2025-06-21 10:57:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "🎉 Hello from Career Compass!"
+   return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
