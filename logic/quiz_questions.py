76fbd3d44e3c436ced50a159f70059b43fc222e5
zooro-07 <suryavs2008@gmail.com>
2025-06-21 19:01:40 +0530

    Create quiz_questions.py

---
+++ logic/quiz_questions.py
@@ -0,0 +1,7 @@
+quiz_questions = [
+    {"q": "Do you enjoy working with numbers?", "v": "math"},
+    {"q": "Are you interested in computers and programming?", "v": "tech"},
+    {"q": "Do you like helping people directly?", "v": "social"},
+    {"q": "Do you enjoy drawing or designing?", "v": "art"},
+    {"q": "Are you passionate about science?", "v": "science"},
+]
